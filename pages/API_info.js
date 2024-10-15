2daab7763ba0898ab8e970296f6f16f7e0311dd5
sramirez124 <forwardemails124@gmail.com>
2024-10-15 00:18:51 -0500

    minor code changes and css changes

--- pages/API_info.js
+++ pages/API_info.js
@@ -3,9 +3,6 @@ import * as pagebuilder from "./pagebuilder.js";
 const API_key = "bdb2824cae258a0c4c646d20255ae299";
 
 export async function getInfo(city) {
-    /**
-     * API says it has an error object but it doesn't exist when using the API
-     */
     const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&cnt={7}&appid=${API_key}&units=imperial`;
 
     const response = await fetch(APIUrl);
@@ -18,7 +15,7 @@ export async function getInfo(city) {
 function sendDisplayInfo(data){
     const {
         name: city, 
-        main: {temp, feels_like, temp_min, temp_max, humidity}, 
+        main: {temp, feels_like, temp_min, temp_max}, 
         weather: [{description, icon}]
     } = data;
     
@@ -28,6 +25,6 @@ function sendDisplayInfo(data){
 
 export function weatherImage(weatherCode) {
     const imgUrl = `http://openweathermap.org/img/wn/${weatherCode}@2x.png`;
-    console.log(imgUrl);
+    // console.log(imgUrl);
     return imgUrl
 }
--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,15 @@ const cityInput = document.querySelector(".cityInput");
 weatherInput.addEventListener("submit", (event) => {
     event.preventDefault();
     const city = cityInput.value;
-    API_info.getInfo(city);
 
-    // if (city) {
-    //     try {
-    //         const getInfo = API_info.getInfo(city);
-    //     }
-    //     catch (error) {
-    //         console.log(error);
-    //     }
-    // } else {
-    //     window.alert("Please enter a US city");
-    // }
+    if (city) {
+        try {
+            const getInfo = API_info.getInfo(city);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    } else {
+        window.alert("Please enter a US city");
+    }
 });
