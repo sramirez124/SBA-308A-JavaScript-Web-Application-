ded9a4d05a2a200cb260d6b1a84ea0edba469e24
sramirez124 <forwardemails124@gmail.com>
2024-10-14 22:53:35 -0500

    switched to new api

--- pages/API_info.js
+++ pages/API_info.js
@@ -1,6 +1,6 @@
 import * as pagebuilder from "./pagebuilder.js";
 
-const API_key = "1d5b6a5b-9a0d-4b6a-9d6a-5b1d5b6a5b6a";
+const API_key = "bdb2824cae258a0c4c646d20255ae299";
 
 /**
  * Hourly Variables
@@ -30,58 +30,15 @@ let dailyPrecipitationProbability = 0;
 
 
 
-getInfo();
-async function getInfo() {
+//getInfo();
+export async function getInfo(city) {
     /**
      * API says it has an error object but it doesn't exist when using the API
      */
-    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
 
-    try{
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=41.9533&longitude=-87.7054&current=temperature_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code&hourly=temperature_2m,precipitation_probability,precipitation&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto");
-        
-        if (response.error == true) {
-            throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        console.log(data);    
-        
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await fetch(APIUrl);
+    const data = await response.json();
+    console.log(data);
 
-
-    // const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=41.9533&longitude=-87.7054&current=temperature_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code&hourly=temperature_2m,precipitation_probability,precipitation&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto");
-    // const data = await response.json();
-
-    /**
-     * Set Current Variables
-     */
-
-    currentTemp = data.current.temperature_2m;
-    console.log("Current Temp: " + currentTemp);
-
-    currentRealFeelTemp = data.current.apparent_temperature;
-    console.log("Current RealFeel Temp: " + currentRealFeelTemp);
-
-    location = data.timezone;
-    pagebuilder.currentWeatherDisplay(location, currentTemp, currentRealFeelTemp);
-
-    /**
-     * Set Hourly Variables
-     * TODO Work in Progress
-     */
-
-    /**
-     * Set Daily Variables
-     */
-    console.log("7 Day Forecast");
-    for (let i = 0; i < data.daily.time.length; i++) {
-        pagebuilder.sevenDayForecast(data.daily.time[i], data.daily.temperature_2m_max[i], data.daily.temperature_2m_min[i], data.daily.precipitation_probability_max[i]);
-
-        console.log(data.daily.time[i], " Max Temp: " + data.daily.temperature_2m_max[i], " Min Temp: " + data.daily.temperature_2m_min[i], " Precipitation Chance: " + data.daily.precipitation_probability_max[i]);
-    }
-
-
-    
 }
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
-//import * as pagebuilder from "pagebuilder.js";
+import * as API_info from "./API_info.js";
+
+const weatherInput = document.querySelector(".weatherInput");
+const cityInput = document.querySelector(".cityInput");
+
+weatherInput.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const city = cityInput.value;
+    
+    if (city) {
+        try {
+            const getInfo = API_info.getInfo(city);
+            console.log(getInfo);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    } else {
+        window.alert("Please enter a US city");
+    }
+});
